dc60e06b6a8a7422285a47a4ab21c18118afffde
Jose Lechuga <joslec9@gmail.com>
2016-10-24 19:34:49 -0400

    update
    update

--- js/application.js
+++ js/application.js
@@ -1,29 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("form").submit(function() {
-
-        // Assign form input values to variables
+    $("form").submit(function () {
         var firstName = $("#first-name").val();
         var lastName = $("#last-name").val();
         var emailAddress = $("#email-address").val();
         var phone = $("#phone").val();
 
-        // Append table to include submitted
-        // input tag values
         $("table").children("tbody").append("<tr><td>" + firstName + "</td><td>" + lastName + "</td><td>" + emailAddress + "</td><td>" + phone + "</td></tr>");
         return false;
-		
     });
 
-    // Reset button
-    $(document).on("click", "button", function() {
-        $("table").find("tbody").remove();
-        $("table").append("<tbody></tbody>");
+    // Alert When Submitted
+    $("form").submit(function () {
+        alert("Thank you for Your Submission!! It will be added now");
     });
-	 
-	// Alert When Submitted
-	$("form").submit(function(){
-    alert("Thank you for Your Submission!! It will be added now");
-});
 
 });
